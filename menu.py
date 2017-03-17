2e349c80dddedaeeb34fd09296680ca4023af2af
cherylyli <cherylliat@gmail.com>
2017-03-17 12:47:19 -0400

    implement get_active_auctions

--- menu.py
+++ menu.py
@@ -1,12 +1,21 @@
 import psycopg2
+import datetime
 
-
-def get_all_auctions():
-	cur.execute("""SELECT * from itemlistings""")
+def get_all_auctions(cur):
+	cur.execute("""select itemid, auctionendtime, itemdescription
+				from itemlistings
+				where auctionendtime > now()
+				order by auctionendtime
+	""")
 	rows = cur.fetchall()
 	print("All ItemListings:")
 	for row in rows:
-	    print(row)
+		print(row)
+
+
+	return
+
+
 
 
 
@@ -18,28 +27,31 @@ def connect_to_db():
 	    cur = conn.cursor()
 	except:
 	    print("I am unable to connect to the database")
+	return cur
 
 
 
 #initialize db and variables
-connect_to_db()
+cur = connect_to_db()
 input_str = ""
 
 # user picks option and execute the option
 while True:
 	input_str = input("Please choose an action:\n" \
 				"1. See all live auctions\n" \
-				"2. Add an item \n" \
-				"3. Place a Bid \n" \
-				"4. See my history \n" \
-				"5. Pay for an item\n"\
+				"2. See detailed auction\n" \
+				"3. Add an item \n" \
+				"4. Place a Bid \n" \
+				"5. See my history \n" \
 				"6. Quit\n")
 	option_int = int(input_str.strip())
-	get_all_auctions()
+	
 
 	# resolve input 
 	if option_int == 6:
 		break
+	elif option_int == 1:
+		get_all_auctions(cur)
 	else:
 		print("You choose {}".format(option_int))
 
